99a6f99751aa6dc834e928089ded36c70c682ef6
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-30 19:41:40 -0500

    fix access token generation and success request test in access tokens controller

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -3,5 +3,10 @@ class AccessTokensController < ApplicationController
   def create 
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform 
+    render json: serializer.new(authenticator.access_token), status: :created
+  end
+
+  def serializer 
+    AccessTokenSerializer
   end
 end
